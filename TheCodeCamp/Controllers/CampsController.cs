a98610caf75e543bdb263db632c046f0ec05ed71
Charly <brianrecinosc@hotmail.com>
2020-08-28 11:18:43 -0400

    Adding changes to controllers and a Get method.

--- TheCodeCamp/Controllers/CampsController.cs
+++ TheCodeCamp/Controllers/CampsController.cs
@@ -1,16 +1,39 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Threading.Tasks;
 using System.Web;
 using System.Web.Http;
+using TheCodeCamp.Data;
 
 namespace TheCodeCamp.Controllers
 {
     public class CampsController : ApiController
     {
-        public object Get()
+        private readonly ICampRepository _repository;
+
+        public CampsController(ICampRepository repository)
         {
-            return new { Name = "Shawn", Occupation = "Teacher" };
+            _repository = repository;
+        }
+
+
+        public async Task<IHttpActionResult> Get()
+        {
+            try
+            {
+                var result = await _repository.GetAllCampsAsync();
+
+                return Ok(result);
+            }
+            catch (Exception ex )
+            {
+                //Todo add logging.
+                return InternalServerError(ex);
+            }
+
+            //adding a static value
+            //return Ok(new { Name = "Shawn", Occupation = "Teacher" });
         }
 
     }
