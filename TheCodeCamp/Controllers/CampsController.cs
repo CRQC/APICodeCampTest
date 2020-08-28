1a422cade03973d7e814e414f3368827fd7c7357
Charly <brianrecinosc@hotmail.com>
2020-08-28 11:01:45 -0400

    Add project files.

---
+++ TheCodeCamp/Controllers/CampsController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Http;
+
+namespace TheCodeCamp.Controllers
+{
+    public class CampsController : ApiController
+    {
+        public object Get()
+        {
+            return new { Name = "Shawn", Occupation = "Teacher" };
+        }
+
+    }
+}
