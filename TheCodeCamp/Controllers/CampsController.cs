be734d364e1234e9147a6e5cf07866822fa784bd
Charly <brianrecinosc@hotmail.com>
2020-08-31 22:54:12 -0400

    correction after merge.

--- TheCodeCamp/Controllers/CampsController.cs
+++ TheCodeCamp/Controllers/CampsController.cs
@@ -73,73 +73,6 @@ namespace TheCodeCamp.Controllers
         
         }
 
-        [Route("searchByDate/{eventDate:datetime}")]
-        [HttpGet]
-        public async Task<IHttpActionResult> SearchByEventDate(DateTime eventDate, bool includeTalks = false) {
-
-            try
-            {
-                var result = await _repository.GetAllCampsByEventDate(eventDate, includeTalks);
-                return Ok(_mapper.Map<CampModel>(result));
-
-            }
-            catch (Exception ex)
-            {
-
-                return InternalServerError(ex);
-            }
-
-        }
-
-        [Route()]
-        public async Task<IHttpActionResult> Get(bool includeTalks = false)
-        {
-            try
-            {
-                var result = await _repository.GetAllCampsAsync(includeTalks);
-
-                if (result == null)
-                {
-                    return NotFound();
-                }
-
-                //mapping
-                var mappedResult = _mapper.Map<IEnumerable<CampModel>>(result);
-
-                return Ok(mappedResult);
-
-                //return Ok(result);
-            }
-            catch (Exception ex )
-            {
-                //Todo add logging.
-                return InternalServerError(ex);
-            }
-
-            //adding a static value
-            //return Ok(new { Name = "Shawn", Occupation = "Teacher" });
-        }
-
-        //if no routing is done. [Route("api/camps/{Moniker}")]
-        [Route("{Moniker}", Name ="GetCamp")]
-        public async Task<IHttpActionResult> Get(string moniker, bool includeTalks = false) {
-
-            try
-            {
-                var result = await _repository.GetCampAsync(moniker, includeTalks);
-                if (result == null)
-                {
-                    return NotFound();
-                }
-                return Ok(_mapper.Map<CampModel>(result));
-            }
-            catch (Exception ex)
-            {
-
-                return InternalServerError(ex);
-            }
-        
-        }
 
         [Route("searchByDate/{eventDate:datetime}")]
         [HttpGet]
@@ -156,7 +89,7 @@ namespace TheCodeCamp.Controllers
 
                 return InternalServerError(ex);
             }
-        
+
         }
 
         [Route()]
--- TheCodeCamp/Models/CampModel.cs
+++ TheCodeCamp/Models/CampModel.cs
@@ -18,9 +18,6 @@ namespace TheCodeCamp.Models
         public DateTime EventDate { get; set; } = DateTime.MinValue;
         [Required]
         [Range(1, 30)]
-        public string Name { get; set; }
-        public string Moniker { get; set; }
-        public DateTime EventDate { get; set; } = DateTime.MinValue;
         public int Length { get; set; } = 1;
 
         public ICollection<TalkModel> Talks { get; set; }
